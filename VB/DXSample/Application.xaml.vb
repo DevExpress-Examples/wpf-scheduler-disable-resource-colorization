92bfa790a59b625198c600501bb9e9f507fdebf8
DevExpressExampleBot <fakeEmail>
2020-06-19 23:42:56 +0300

    Source auto update [skip ci]

---
+++ VB/DXSample/Application.xaml.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Collections.Generic
+Imports System.Windows
+Imports DevExpress.Xpf.Core
+
+Namespace DXSample
+	Partial Public Class App
+		Inherits Application
+
+		Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+			HideClassicThemes()
+			MyBase.OnStartup(e)
+		End Sub
+		Private Sub HideClassicThemes()
+			Dim ModernThemeCaterogies As New List(Of String)() From {Theme.Office2016Category, Theme.Office2016SECategory, Theme.Office2019Category, Theme.VisualStudioCategory, Theme.MetropolisCategory}
+
+			For Each theme As Theme In Theme.Themes
+				theme.ShowInThemeSelector = ModernThemeCaterogies.Contains(theme.Category)
+			Next theme
+		End Sub
+	End Class
+End Namespace
